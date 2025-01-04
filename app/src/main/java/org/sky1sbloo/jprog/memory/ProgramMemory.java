99f0eeeb0ec525753a8428790dad93606cbeaf0d
Sky1sBloo <ultimate8gaming@gmail.com>
2025-01-04 10:17:24 +0800

    Rewritten program memory to handle its own allocation

--- app/src/main/java/org/sky1sbloo/jprog/memory/ProgramMemory.java
+++ app/src/main/java/org/sky1sbloo/jprog/memory/ProgramMemory.java
@@ -7,9 +7,13 @@ import java.util.*;
  */
 public class ProgramMemory {
     private final Deque<HashMap<Integer, MemoryCell>> memoryStack;
+    private final Deque<Queue<Integer>> unusedPtrs;  // Represents the deallocated pointers
+    private final Deque<Integer> nextPtr;  // Represents the ptr that will be allocated next based on the stack frame
 
     public ProgramMemory() {
-        this.memoryStack = new ArrayDeque<>();
+        this.memoryStack = new LinkedList<>();
+        this.nextPtr = new LinkedList<>();
+        this.unusedPtrs = new LinkedList<>();
         allocateStackFrame();
     }
 
@@ -19,6 +23,8 @@ public class ProgramMemory {
     public void allocateStackFrame() {
         memoryStack.addFirst(new HashMap<>());
         memoryStack.push(new HashMap<>());
+        unusedPtrs.push(new LinkedList<>());
+        nextPtr.push(0);
     }
 
     /**
@@ -29,13 +35,40 @@ public class ProgramMemory {
             throw new IllegalStateException("Tried to deallocate the global stack frame");
         }
         memoryStack.pop();
+        unusedPtrs.pop();
+        nextPtr.pop();
+    }
+
+    /**
+     * Allocates a new memory cell
+     * @return The memory address of the cell
+     */
+    public int allocateMemoryCell(MemoryCell cell) {
+        if (memoryStack.isEmpty() || nextPtr.isEmpty() || unusedPtrs.isEmpty()) {
+            throw new IllegalStateException("Tried to allocate on empty stack frame");
+        }
+
+        if (!unusedPtrs.peek().isEmpty()) {
+            Integer address = unusedPtrs.peek().poll();
+            if (address == null) {
+                throw new IllegalStateException("Address on allocation is null");
+            }
+            setMemoryCell(address, cell);
+            return address;
+        }
+
+        int address = nextPtr.peek();
+        setMemoryCell(address, cell);
+        nextPtr.pop();
+        nextPtr.push(address + 1);
+        return address;
     }
 
     /**
      * Sets the memory cell at the given address
      */
     public void setMemoryCell(int address, MemoryCell cell) {
-        if (memoryStack.peek() == null) {
+        if (memoryStack.isEmpty()) {
             throw new IllegalStateException("Tried to allocate on empty stack frame");
         }
         memoryStack.peek().put(address, cell);
@@ -45,7 +78,7 @@ public class ProgramMemory {
      * Frees the memory cell at the given address
      */
     public void freeMemoryCell(int address) {
-        if (memoryStack.peek() == null) {
+        if (memoryStack.isEmpty() || unusedPtrs.isEmpty()) {
             throw new IllegalStateException("Tried to deallocate on empty stack frame");
         }
         try {
@@ -53,5 +86,7 @@ public class ProgramMemory {
         } catch (NoSuchElementException e) {
             throw new IllegalStateException("Tried to deallocate an undefined cell");
         }
+
+        unusedPtrs.peek().add(address);
     }
 }
