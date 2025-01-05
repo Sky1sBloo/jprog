999b622adc861d6febf7c219cd4d9dc70d6d721f
Sky1sBloo <ultimate8gaming@gmail.com>
2025-01-05 14:47:04 +0800

    Added initial functions for interpreter

--- app/src/main/java/org/sky1sbloo/jprog/interpreter/Interpreter.java
+++ app/src/main/java/org/sky1sbloo/jprog/interpreter/Interpreter.java
@@ -1,11 +1,52 @@
 package org.sky1sbloo.jprog.interpreter;
 
+import org.sky1sbloo.jprog.memory.*;
 import org.sky1sbloo.jprog.syntaxtree.ParseNodes;
+import org.sky1sbloo.jprog.syntaxtree.ParseNodesException;
+import org.sky1sbloo.jprog.syntaxtree.ParseNodesVisitor;
 
 /**
  * Class for interpreting the syntax tree
  */
 public class Interpreter {
-    public Interpreter(ParseNodes.Expr root) {
+    private final VariableHandler variableHandler;
+    private final FunctionHandler functionHandler;
+
+    public Interpreter(ParseNodes.Expr root, VariableHandler variableHandler, FunctionHandler functionHandler) {
+        this.variableHandler = variableHandler;
+        this.functionHandler = functionHandler;
+    }
+
+    private void performVariableInitialization(ParseNodes.VariableInitializationExpr expr) {
+        variableHandler.allocateVariable(expr.identifier());
+    }
+
+    private void performVariableAssignment(ParseNodes.AssignmentExpr expr) {
+        try {
+            variableHandler.setVariable(expr.identifier(),
+                    visitValueExpr(expr.value()));
+        } catch (RuntimeException e) {
+            throw new InterpreterException("Failed to interpret ParseTree: " + e.getMessage());
+        }
+    }
+
+    MemoryCell visitValueExpr(ParseNodes.Value expr) throws RuntimeException {
+        return ParseNodesVisitor.visitValue(expr,
+                (literalExpr) -> {
+                    try {
+                        return MemoryCells.build(literalExpr.value());
+                    } catch (WrongTypeException e) {
+                        throw new RuntimeException(e);
+                    }
+                },
+                (identifierExpr) -> variableHandler.getVariable(identifierExpr.value()),
+                (binaryExpr) -> {
+                    //  Todo: add implementation
+                    return null;
+                },
+                (functionCallExpr) -> {
+                    // Todo: add implementation
+                    return null;
+                });
     }
 }
