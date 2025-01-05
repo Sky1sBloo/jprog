2d0aae57975a577c044395e24c7e5b0a0fd4bc49
Sky1sBloo <ultimate8gaming@gmail.com>
2025-01-05 14:36:33 +0800

    Renamed visit function and added separate visit value and terminal expr

--- app/src/main/java/org/sky1sbloo/jprog/syntaxtree/ParseNodesVisitor.java
+++ app/src/main/java/org/sky1sbloo/jprog/syntaxtree/ParseNodesVisitor.java
@@ -6,14 +6,19 @@ import java.util.function.Function;
  * Class for visiting parse nodes
  */
 public class ParseNodesVisitor {
-    public static <R> R visitValueExpr(ParseNodes.Expr expr,
-                                       Function<ParseNodes.LiteralExpr, R> literalExprFunction,
-                                       Function<ParseNodes.IdentifierExpr, R> identifierExprFunction,
-                                       Function<ParseNodes.BinaryExpr, R> binaryExprFunction,
-                                       Function<ParseNodes.VariableInitializationExpr, R> variableInitializationExprFunction,
-                                       Function<ParseNodes.AssignmentExpr, R> assignmentExprFunction,
-                                       Function<ParseNodes.FunctionCallExpr, R> functionCallExprFunction,
-                                       Function<ParseNodes.FunctionDefinitionExpr, R> functionDefinitionExprFunction) {
+    /**
+     * Visits all valid expr types and runs the corresponding function
+     *
+     * @param expr Expression node to be checked
+     */
+    public static <R> R visit(ParseNodes.Expr expr,
+                              Function<ParseNodes.LiteralExpr, R> literalExprFunction,
+                              Function<ParseNodes.IdentifierExpr, R> identifierExprFunction,
+                              Function<ParseNodes.BinaryExpr, R> binaryExprFunction,
+                              Function<ParseNodes.VariableInitializationExpr, R> variableInitializationExprFunction,
+                              Function<ParseNodes.AssignmentExpr, R> assignmentExprFunction,
+                              Function<ParseNodes.FunctionCallExpr, R> functionCallExprFunction,
+                              Function<ParseNodes.FunctionDefinitionExpr, R> functionDefinitionExprFunction) {
         if (expr instanceof ParseNodes.LiteralExpr literalExpr) {
             return literalExprFunction.apply(literalExpr);
         }
@@ -36,5 +41,50 @@ public class ParseNodesVisitor {
             return functionDefinitionExprFunction.apply(functionDefinitionExpr);
         }
         throw new ParseNodesException("Expression is not a valid parse node");
+    }
+
+    /**
+     * Visits all valid value expr and calls the corresponding function
+     * @param expr Expression to checked
+     */
+    public static <R> R visitValue(ParseNodes.Value expr,
+                                   Function<ParseNodes.LiteralExpr, R> literalExprFunction,
+                                   Function<ParseNodes.IdentifierExpr, R> identifierExprFunction,
+                                   Function<ParseNodes.BinaryExpr, R> binaryExprFunction,
+                                   Function<ParseNodes.FunctionCallExpr, R> functionCallExprFunction) {
+        if (expr instanceof ParseNodes.LiteralExpr literalExpr) {
+            return literalExprFunction.apply(literalExpr);
+        }
+        if (expr instanceof ParseNodes.IdentifierExpr identifierExpr) {
+            return identifierExprFunction.apply(identifierExpr);
+        }
+        if (expr instanceof ParseNodes.BinaryExpr binaryExpr) {
+            return binaryExprFunction.apply(binaryExpr);
+        }
+        if (expr instanceof ParseNodes.FunctionCallExpr functionCallExpr) {
+            return functionCallExprFunction.apply(functionCallExpr);
+        }
+        throw new ParseNodesException("Expression is not a valid value parse node");
+    }
+
+    /**
+     * Visits all valid terminal expr types and runs the corresponding function
+     *
+     * @param expr Expression to be checked
+     */
+    public static <R> R visitTerminal(ParseNodes.Terminal expr,
+                              Function<ParseNodes.VariableInitializationExpr, R> variableInitializationExprFunction,
+                              Function<ParseNodes.AssignmentExpr, R> assignmentExprFunction,
+                              Function<ParseNodes.FunctionDefinitionExpr, R> functionDefinitionExprFunction) {
+        if (expr instanceof ParseNodes.VariableInitializationExpr variableInitializationExpr) {
+            return variableInitializationExprFunction.apply(variableInitializationExpr);
+        }
+        if (expr instanceof ParseNodes.AssignmentExpr assignmentExpr) {
+            return assignmentExprFunction.apply(assignmentExpr);
+        }
+        if (expr instanceof ParseNodes.FunctionDefinitionExpr functionDefinitionExpr) {
+            return functionDefinitionExprFunction.apply(functionDefinitionExpr);
+        }
+        throw new ParseNodesException("Expression is not a valid terminal parse node");
     }
 }
