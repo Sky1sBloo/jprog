3cd0278236b6d9da57ddf36d161057ed4a09a8a0
Sky1sBloo <ultimate8gaming@gmail.com>
2025-01-05 14:01:43 +0800

    feat: Added parse nodes exception

---
+++ app/src/main/java/org/sky1sbloo/jprog/syntaxtree/ParseNodesException.java
@@ -0,0 +1,7 @@
+package org.sky1sbloo.jprog.syntaxtree;
+
+public class ParseNodesException extends RuntimeException {
+    public ParseNodesException(String message) {
+        super(message);
+    }
+}
